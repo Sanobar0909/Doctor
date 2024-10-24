45e371ca43af45a543b87fc4b64983b7460d7c97
Shahnoza2013 <davlatergoziev78@gmail.com>
2024-10-24 13:04:39 +0500

    article

--- src/main/java/uz/pdp/Doctor/config/OpenApiConfig.java
+++ src/main/java/uz/pdp/Doctor/config/OpenApiConfig.java
@@ -1,5 +1,6 @@
 package uz.pdp.Doctor.config;
 
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
